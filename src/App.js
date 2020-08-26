f46a776b63a8ed54cfc74b12c94ae1074b6284fb
Rodion Golovushkin <rodion.golovushkin@atnr.pro>
2020-08-26 19:08:14 +0700

    add prettier support

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import { Button } from '@material-ui/core';
 import './App.css';
 
 function App() {
-  return (
-    <Button color="primary">Hello World</Button>
-  );
+  return <Button color="primary">Hello World</Button>;
 }
 
 export default App;
